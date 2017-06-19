fafe6325f6f08c0413accf6a32d820f43ded4f34
Alessa <kmanankov@bk.ru>
2017-06-19 23:07:21 +0300

    mmenu gropdown

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
 $(function() {
-
-	// Custom JS
-
+    function clog(item) {
+        console.log(item);
+    }
+    
+    
+    //$(".sf-menu").after("<div id='my-menu'>"); //создаем блок для меню
+    $(".sf-menu").clone().appendTo("#my-menu"); //клонируем меню с шапки в мобильное меню
+    $("#my-menu").find("*").attr("style", ""); //очищаем от встроеных стилей
+    $("#my-menu").find("ul").removeClass("sf-menu"); //очищаем от встроеных стилей
+    $('#my-menu').mmenu({
+        dropdown: true, //меню ниже панели
+        slidingSubmenus: false,//открытие подменю внутри вниз
+        extensions: ["border-none", "theme-white"],
+        offCanvas: {
+            position: "left" //открывать слева
+        },
+        navbar: {
+            title: "" //убираем название в меню сверху
+        }
+    });
+    //Крестик-гамбургер https://jonsuh.com/hamburgers/
+    var api = $("#my-menu").data("mmenu");
+    api.bind("open:start",function(){ //когда меню Открыто
+        $(".hamburger").addClass("is-active"); //добавить класс, Появится крестик
+    });
+    api.bind("close:before",function(){
+        $(".hamburger").removeClass("is-active"); //без класса будет гамбургер
+    });
+   
+  
 });
--- gulpfile.js
+++ gulpfile.js
@@ -58,11 +58,13 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/jQuery.mmenu/dist/jquery.mmenu.all.js', //мобильное меню
 		'app/libs/owl.carousel/dist/owl.carousel.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Минимизировать весь js (на выбор)
+	.pipe(gulp.dest('app/js'))
+	.pipe(uglify()) // Минимизировать весь js (на выбор)
 	.pipe(gulp.dest('app/js'))
 	.pipe(browserSync.reload({stream: true}));
 });
@@ -81,9 +83,10 @@ gulp.task('browser-sync', function() {
 gulp.task('sass', function() {
 	return gulp.src('app/sass/**/*.sass')
 	.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
-	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(autoprefixer(['last 15 versions']))
-	//.pipe(cleanCSS()) // Опционально, закомментировать при отладке
+	.pipe(gulp.dest('app/css'))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.reload({stream: true}));
 });
